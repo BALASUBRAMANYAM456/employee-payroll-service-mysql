465dbe671790030c2acc59a476e80b491974ad97
subramanyam <sbalasubramanyam98@gmail.com>
2022-09-22 12:26:12 +0530

    Sorted entries alphabetically by person's name.

---
+++ AddressBookList.java
@@ -0,0 +1,40 @@
+package com.addressbooksystem;
+
+import java.util.ArrayList;
+
+public class AddressBookList {
+	
+	String userInputBookName;
+	ArrayList<Contact_info> contact;
+
+	
+	// constructor
+	public AddressBookList(String userInputBookName) {
+		this.userInputBookName = userInputBookName;
+		this.contact = new ArrayList<Contact_info>();
+	}
+
+	// getters and methods.
+	public String getUserInputBookName() {
+		return userInputBookName;
+	}
+
+	public void setUserInputBookName(String userInputBookName) {
+		this.userInputBookName = userInputBookName;
+	}
+	
+	
+	public ArrayList<Contact_info> getContact() {
+		return contact;
+	}
+
+	public void setContact(ArrayList<Contact_info> contact) {
+		this.contact = contact;
+	}
+
+	/*** Overriding toString() method ***/
+	@Override
+	public String toString() {
+		return "Address Book name :- " + userInputBookName + "\n" + "Contacts :- " + contact;
+	}
+}
