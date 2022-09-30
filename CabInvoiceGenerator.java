d7c141d6b3a00c8cec3a456a8f4350edc5eb6c93
subramanyam <sbalasubramanyam98@gmail.com>
2022-09-30 17:04:25 +0530

    method to get minimum price and writter j unit test cases

--- CabInvoiceGenerator.java
+++ CabInvoiceGenerator.java
@@ -8,14 +8,8 @@ public class CabInvoiceGenerator {
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-        return Math.max(totalFare, MINIMUM_FARE);
-    }
-
-    public InvoiceSummery calculateFare(Ride[] rides) {
-        double totalFare = 0;
-        for (Ride ride : rides) {
-            totalFare += this.calculateFare(ride.distance, ride.time);
-        }
-        return new InvoiceSummery(rides.length, totalFare);
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
